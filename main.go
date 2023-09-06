5cf55e7e0857975f647404c69f33f051df8095b2
yeyo27 <yeyo1mm@gmail.com>
2023-09-06 23:56:43 +0200

    Root endpoint receives name parameter

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func indexHandler(c *gin.Context) {
+func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
 	c.JSON(200, gin.H{
-		"message": "hello world",
+		"message": "hello " + name,
 	})
 }
 
 
 func main() {
 	router := gin.Default()
-	router.GET("/", indexHandler)
+	router.GET("/:name", IndexHandler)
 	router.Run()
 }
