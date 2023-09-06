a2785f58ace813e5e0fa5f17789ca86bd0317b69
yeyo27 <yeyo1mm@gmail.com>
2023-09-07 01:10:36 +0200

    Added XML handler for /name endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/xml"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,9 +13,24 @@ func IndexHandler(c *gin.Context) {
 	})
 }
 
+type Person struct {
+	XMLName xml.Name `xml:"person"`
+	FirstName string `xml:"firstName,attr"`
+	LastName string `xml:"lastName,attr"`
+}
+
+func IndexHandlerXML(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.XML(200, Person{
+		FirstName: name,
+		LastName: "mesa",
+	})
+}
+
 
 func main() {
 	router := gin.Default()
 	router.GET("/:name", IndexHandler)
+	router.GET("/:name/xml", IndexHandlerXML)
 	router.Run()
 }
