a4106b11b1bebb5005d09506a294b098297fd86d
yeyo27 <yeyo1mm@gmail.com>
2023-09-09 02:30:55 +0200

    new recipew endpoint

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
+	"net/http"
 	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
-
-
-func IndexHandler(c *gin.Context) {
-	name := c.Params.ByName("name")
-	c.JSON(200, gin.H{
-		"message": "hello " + name,
-	})
-}
 
 
 type Recipe struct {
@@ -23,9 +18,55 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+var recipesDB []Recipe
+
+func init() {
+	recipesDB = make([]Recipe, 0)
+}
+
+
+func IndexHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hosting recipes app",
+	})
+}
+
+
+func GetRecipesHandler(c *gin.Context) {
+
+}
+
+
+func PostRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipesDB = append(recipesDB, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
+
+func PutRecipesHandler(c *gin.Context) {
+
+}
+
+func DeleteRecipesHandler(c *gin.Context) {
+
+}
+
 
 func main() {
 	router := gin.Default()
-	router.GET("/:name", IndexHandler)
+	router.GET("/", IndexHandler)
+	router.GET("/recipes", GetRecipesHandler)
+	router.POST("/recipes", PostRecipeHandler)
+	router.PUT("/recipes/:id", PutRecipesHandler)
+	router.DELETE("/recipes/:id", DeleteRecipesHandler)
 	router.Run()
 }
